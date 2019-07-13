0fa159fc77665869e8a391e7cd70cc352400106c
Qiu Wenyuan <42402590+WenyuanQ@users.noreply.github.com>
2019-07-13 22:43:51 +0800

    Add files via upload

---
+++ session.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+matrix_1 = tf.constant([[3,3]])
+matrix_2 = tf.constant([[2],[2]])
+
+product = tf.matmul(matrix_1,matrix_2)
+print(product)
+
+#method 1
+# sess = tf.Session()
+# result = sess.run(product)
+# print(result)
+# sess.close()
+
+#method 2
+with tf.Session() as sess:
+    result2 = sess.run(product)
+    print(result2)
+print("fini")
---
+++ test.py
@@ -0,0 +1,20 @@
+'''L = [1,2,3,1,2,3,1,4,2,1,2,2,1,1,4,1,]
+
+out = set(L)
+#out.sort()
+t = len(out)
+print(t,out,list(out))'''
+
+# import numpy as np
+
+# a = [[3,3]]
+# b = [[2],[2]]
+
+# c = np.dot(a,b)
+
+# print(c)
+
+#[]
+num = 1.22
+d = [num]
+print(d,type(d))
